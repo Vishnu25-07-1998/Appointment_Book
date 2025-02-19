59d1d007e12509f759897f2a0d6b46cedd59b2ac
Vishnu25-07-1998 <156580998+Vishnu25-07-1998@users.noreply.github.com>
2025-02-19 14:44:22 +0530

    Update urls.py

--- Book_Appointment/urls.py
+++ Book_Appointment/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path("get-slots/", views.get_available_slots, name="get_slots"),
     path("book-appointment/", views.book_appointment, name="book_appointment"),
+    path("get_appointments/", views.get_appointments_by_date, name="get_appointments"),
 ]
